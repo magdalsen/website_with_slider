167c3d19821c6e61c0d45d00dca9624c66c2ed1a
Magdalena Senger <magdalenasenger@MacBook-Air-Mac.local>
2023-08-29 11:12:43 +0200

    add custom fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './index.scss'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import { extendTheme } from '@chakra-ui/react'
 import { Button } from './components/Button.tsx'
-
+import { Fonts } from './components/Fonts.tsx'
 
 const navButton = extendTheme({
   components: {
     Button,
+  },
+  fonts: {
+    body: "Montserrat"
   },
 })
 
@@ -18,6 +21,7 @@ const navButton = extendTheme({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={navButton}>
+      <Fonts />
       <App />
     </ChakraProvider>
   </React.StrictMode>,
